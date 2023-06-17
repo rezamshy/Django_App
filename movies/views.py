75343ee03c7e3b38173605374854c819343c5a80
Reza Mashayekhi <rezamashayekhi22@yahoo.com>
2023-06-17 12:45:47 -0600

    Pass data to view

--- movies/views.py
+++ movies/views.py
@@ -4,3 +4,4 @@ from django.views.generic import TemplateView
 # Create your views here.
 class MoviesView(TemplateView):
     template_name = 'movies.html'
+    extra_context = {'movies': 'https://www.omdbapi.com/src/poster.jpg'}
