803244c032e22e27b14dacb7b01e51c0bbb66bb8
Reza Mashayekhi <rezamashayekhi22@yahoo.com>
2023-06-17 16:32:32 -0600

    Add Movies model and register in admin

--- home/views.py
+++ home/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
-class MoviesView(TemplateView):
-    template_name = 'movies/movies.html'
+class HomeView(TemplateView):
+    template_name = 'home/home.html'
     extra_context = {'movies': 'https://www.omdbapi.com/src/poster.jpg'}
--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+class MoviesAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.models, MoviesAdmin)
