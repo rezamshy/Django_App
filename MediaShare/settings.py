acf307b3cccc284943eff6372ae1bec1ea9064b1
Reza Mashayekhi <rezamashayekhi22@yahoo.com>
2023-06-18 13:14:43 -0600

    Use base.html

--- MediaShare/settings.py
+++ MediaShare/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'MediaShare.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'static/templates'],
+        'DIRS': [BASE_DIR / 'static/templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRs = [BASE_DIR / 'static',]
+STATICFILES_DIRS = [BASE_DIR / 'static',]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
