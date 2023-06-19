30020405a6bc43c6e6058ce7af7605aa628f8e13
Reza Mashayekhi <rezamashayekhi22@yahoo.com>
2023-06-18 21:52:21 -0600

    Update style

--- movies/urls.py
+++ movies/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MoviesView.as_view()),
+    path('', views.MoviesView.as_view(), name = 'movies.list'),
     path('create', views.CreateMovieView.as_view(), name = 'movies.create'),
 ]
